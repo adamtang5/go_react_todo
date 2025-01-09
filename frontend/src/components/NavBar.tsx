78a78d67e12d1a0d0ee88b5d3751f62280f4525f
Adam Tang <adamtang@gmail.com>
2025-01-08 23:15:05 -0800

    styled Box component

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,11 +1,18 @@
-import { Container, useColorMode } from '@chakra-ui/react'
+import { Box, Container, useColorMode, useColorModeValue } from '@chakra-ui/react'
 
 function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
     <Container maxW="900px">
+      <Box
+        bg={useColorModeValue("gray.400", "gray.700")}
+        px={4}
+        my={4}
+        borderRadius="5"
+      >
 
+      </Box>
     </Container>
   )
 }
