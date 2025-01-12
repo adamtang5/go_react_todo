2bf163f345c4d21d7ed2f57e6064030489157871
Adam Tang <adamtang@gmail.com>
2025-01-11 17:30:30 -0800

    handling mode for deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,9 @@ import NavBar from './components/NavBar'
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
 
-export const BASE_URL = "http://localhost:4000/api"
+export const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:4000/api"
+  : "/api"
 
 function App() {
   return (
