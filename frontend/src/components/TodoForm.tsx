f6d4ae8bb63a7368acce5aceb59f8c38369459ba
Adam Tang <adamtang@gmail.com>
2025-01-11 16:46:12 -0800

    implemented createTodo

--- frontend/src/components/TodoForm.tsx
+++ frontend/src/components/TodoForm.tsx
@@ -1,15 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { BASE_URL } from "../App"
 import { Button, Flex, Input, Spinner } from "@chakra-ui/react"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useState } from "react"
 import { IoMdAdd } from "react-icons/io"
 
 function TodoForm() {
   const [newTodo, setNewTodo] = useState("")
-  const [isPending, setIsPending] = useState(false)
+  const queryClient = useQueryClient()
 
-  const createTodo = async (e: React.FormEvent) => {
-    e.preventDefault()
-    alert("Todo added!")
-  }
+  const { mutate: createTodo, isPending: isCreating } = useMutation({
+    mutationKey: ["createTodo"],
+    mutationFn: async (e: React.FormEvent) => {
+      e.preventDefault()
+      try {
+        const res = await fetch(BASE_URL + `/todos`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ body: newTodo }),
+        })
+        const data = await res.json()
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong")
+        }
+
+        setNewTodo("")
+        return data
+      } catch (err: any) {
+        throw new Error(err)
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] })
+    },
+    onError: (err: any) => alert(err.message),
+  })
 
   return (
     <form onSubmit={createTodo}>
@@ -25,7 +53,7 @@ function TodoForm() {
           type="submit"
           _active={{ transform: "scale(.97)" }}
         >
-          {isPending
+          {isCreating
             ? <Spinner size="xs" />
             : <IoMdAdd size={30} />}
         </Button>
